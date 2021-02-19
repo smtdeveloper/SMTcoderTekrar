526f55b647b1182a7d8928584c1282a90aabf628
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-02-20 01:06:42 +0300

    20 / 02 / 2021 - MyDictionary

---
+++ MyDictionary/DictionaryManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class DictionaryManager<K , V>
+    {
+        K[] _keys;
+        V[] _values;
+
+        public DictionaryManager()
+        {
+
+            _keys = new K[0];
+            _values = new V[0];
+        }
+
+        public void Add(K key ,V value)
+        {
+            K[] _tempKey = _keys;
+            V[] _tempValues = _values;
+
+            _keys = new K[_keys.Length + 1];
+            _values = new V[_values.Length + 1];
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _keys[i] = _tempKey[i];
+                _values[i] = _tempValues[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+
+        }
+   
+       
+
+        public void listeleme()
+        {
+            for (int a = 0; a < _keys.Length; a++)
+            {
+                Console.WriteLine(" Key : {0} | Value : {1} ", _keys[a] , _values[a]);
+            }
+
+        }
+
+      
+
+        public int Length
+        {
+            get { return _keys.Length; }
+           
+        }
+
+       
+
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryManager<int, string> takmaisim = new DictionaryManager<int, string>();
+            takmaisim.Add(43,"Kütahya");
+            takmaisim.Add(10, "Balıkkesir");
+            takmaisim.Add(09, "Aydın");
+
+            takmaisim.listeleme();
+
+            Console.WriteLine("---------------------------  ");
+            Console.WriteLine("SMTcoder");
+        }
+    }
+}
