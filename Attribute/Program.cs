0026a8de973f04eafd8dd558002ccb54dc5b510a
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-03-10 14:51:01 +0300

    Reflections - Attributes

---
+++ Attribute/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Attribute
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer = new Customer { Id = 1, LastName = "AKCA", Age = 22 };
+
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine(); 
+            Console.WriteLine("SMTcoder");
+
+        }
+    }
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+       [RequiredProperty]   // Zorunlu alan -- attribute ile yapımı 
+        public string FirstName { get; set; }
+      
+        public string LastName { get; set; }
+       
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0} , {1} , {2} , {3} Eklendi ! " , 
+                 customer.Id , customer.FirstName , customer.LastName , customer.Age);
+        }
+    }
+
+     class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+
+    class ToTableAttribute :Attribute
+    {
+
+
+    }
+
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,97 @@
+﻿    using System;
+using System.Dynamic;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DortIslem dortIslem = new DortIslem(2, 3);
+            //Console.WriteLine(dortIslem.Topla(5, 5));
+            //Console.WriteLine(dortIslem.Topla2());
+
+
+            // Reflection yazılısı ve kullanısı  
+
+            var tip = typeof(DortIslem);
+
+            //DortIslem dortIslem =(DortIslem)Activator.CreateInstance(tip,2,3);
+            //Console.WriteLine(" Topla  : " + dortIslem.Topla( 5,5));
+            //Console.WriteLine(" Topla2 : " + dortIslem.Topla2());
+
+            var intance = Activator.CreateInstance(tip, 5, 6);
+            MethodInfo methodInfo = intance.GetType().GetMethod("Topla2");
+            Console.WriteLine(" intance Topla2 : " + methodInfo.Invoke(intance, null));
+
+            Console.WriteLine("----------");
+
+            var metodlar = tip.GetMethods();
+
+            foreach (var info in metodlar)
+            {
+                Console.WriteLine("Metod Adı : {0}" , info.Name);
+                foreach (var Parametlere in info.GetParameters())
+                {
+                    Console.WriteLine(" Parametlere : {0}" , Parametlere.Name);
+                }
+                foreach (var attribute in info.GetCustomAttributes())
+                {
+                    Console.WriteLine(" Attribute : {0}" , attribute.GetType().Name);
+                }
+            }
+
+            Console.ReadLine();
+          
+        }
+
+
+        public class DortIslem
+        {
+           private int _sayi1;
+           private int _sayi2;
+            public DortIslem(int sayi1, int sayi2)
+            {
+                _sayi1 = sayi1;
+                _sayi2 = sayi2;
+            }
+
+            public int Topla(int sayi1 , int sayi2)
+            {
+                return sayi1 + sayi2; 
+
+            }
+
+            public int Carp(int sayi1, int sayi2)
+            {
+                return sayi1 * sayi2;
+
+            }
+
+            public int Topla2()
+            {
+                return _sayi1 + _sayi2;
+
+            }
+             
+            [MetodName("Carma")]  // attribute
+            public int Carp2()
+            {
+                return _sayi1 * _sayi2;
+
+            }
+
+        }
+
+    }
+
+    class MetodNameAttribute : Attribute
+    {
+        public MetodNameAttribute(string name)
+        {
+
+        }
+    }
+
+}
